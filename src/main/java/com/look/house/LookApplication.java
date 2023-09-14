848a6976ef695361553c7c6e4b7b9f9a81f59ade
minseo0591 <minseo0591@naver.com>
2023-09-14 12:52:33 +0900

    basic upload

---
+++ src/main/java/com/look/house/LookApplication.java
@@ -0,0 +1,13 @@
+package com.look.house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/look/house/LookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.look.house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
