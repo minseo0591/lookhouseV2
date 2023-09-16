72e753f107f8e9cdab7ec67114160e71ac913dde
auh99 <auh9904@gmail.com>
2023-09-17 01:01:54 +0900

    @feat: entity(board, comment, like 설계) + member

---
+++ src/main/java/com/look/house/domain/entity/Like.java
@@ -0,0 +1,21 @@
+package com.look.house.domain.entity;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long likeId;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
